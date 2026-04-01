0b998b248f0c2dbed08f102baaa481c37d76e1b8
Artur Golovko <arturgolovko37@gmail.com>
2026-04-01 13:14:30 +0300

    new dont

--- run.py
+++ run.py
@@ -1,21 +1,4 @@
-import os
-import threading
-from flask import Flask
 from main import main
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Bot is running!"
-
-def run_bot():
-    main()
-
 if __name__ == "__main__":
-    # запускаем бота в отдельном потоке
-    threading.Thread(target=run_bot).start()
-
-    # запускаем веб-сервер
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    main()
